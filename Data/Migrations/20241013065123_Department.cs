4bafa51ba89748927315954a15699e62bbb8f4dd
Abhishek <Abhishekgwari14sep@gmail.com>
2024-10-14 23:40:35 +0530

    Add project files.

---
+++ Data/Migrations/20241013065123_Department.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Department : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department:UserActivity
+
+    {
+        public int Id { get; set; } 
+        public string code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
